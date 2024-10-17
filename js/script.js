05d3c05509d552e96d326c5a8371e26f5476dec6
Shreyas A S <shreyas.as@techversantinfotech.com>
2024-10-17 11:56:15 +0530

    Convert contents of validate function to jquery

--- js/script.js
+++ js/script.js
@@ -62,63 +62,62 @@ function validatePassword(password) {
 }
 
 function validate() {
-	const fullname = document.getElementById("fullname").value;
-	const male = document.getElementById("male").checked;
-	const female = document.getElementById("female").checked;
-	const email = document.getElementById("email").value;
-	const phone = document.getElementById("phone").value;
-	const age = document.getElementById("age").value;
-	const password = document.getElementById("password").value;
-	const confirmPassword = document.getElementById("confirm-password").value;
+	const fullname = $("#fullname").val();
+	const gender = $("input[name='gender']:checked").val();
+	const email = $("#email").val();
+	const phone = $("#phone").val();
+	const age = $("#age").val();
+	const password = $("#password").val();
+	const confirmPassword = $("#confirm-password").val();
 
-	let errorFullname = document.getElementById("error-fullname");
-	let errorGender = document.getElementById("error-gender");
-	let errorEmail = document.getElementById("error-email");
-	let errorPhone = document.getElementById("error-phone");
-	let errorAge = document.getElementById("error-age");
-	let errorPassword = document.getElementById("error-password");
-	let errorConfirmPassword = document.getElementById("error-confirm-password");
+	const errorFullname = $("#error-fullname");
+	const errorGender = $("#error-gender");
+	const errorEmail = $("#error-email");
+	const errorPhone = $("#error-phone");
+	const errorAge = $("#error-age");
+	const errorPassword = $("#error-password");
+	const errorConfirmPassword = $("#error-confirm-password");
 
-	errorFullname.textContent = "";
-	errorGender.textContent = "";
-	errorEmail.textContent = "";
-	errorPhone.textContent = "";
-	errorAge.textContent = "";
-	errorPassword.textContent = "";
-	errorConfirmPassword.textContent = "";
+	errorFullname.text("");
+	errorGender.text("");
+	errorEmail.text("");
+	errorPhone.text("");
+	errorAge.text("");
+	errorPassword.text("");
+	errorConfirmPassword.text("");
 
 	// Name validation
 	if(!validateFullname(fullname)) {
-		errorFullname.textContent = "Not a valid name";
+		errorFullname.text("Not a valid name");
 	}
 
 	// Gender validation
-	if(!male && !female) {
-		errorGender.textContent = "Please select a gender";
+	if(!gender) {
+		errorGender.text("Please select a gender");
 	}
 
 	// Email validation
 	if(!validateEmail(email)) {
-		errorEmail.textContent = "Not a valid email address";
+		errorEmail.text("Not a valid email address");
 	}
 
 	// Phone number validation
 	if(!validatePhoneNum(phone)) {
-		errorPhone.textContent = "Not a valid phone number";
+		errorPhone.text("Not a valid phone number");
 	}
 
 	// Age validation
 	if(age > 60 || age < 18) {
-		errorAge.textContent = "Age should be between 18 and 60";
+		errorAge.text("Age should be between 18 and 60");
 	}
 
 	// Password validation
 	if(!validatePassword(password)) {
-		errorPassword.textContent = "Password must contain the following: A lowercase letter, an uppercase letter, a number, minimum of 8 characters and a special character (@$!%*?&)";
+		errorPassword.text("Password must contain the following: A lowercase letter, an uppercase letter, a number, minimum of 8 characters and a special character (@$!%*?&)");
 	}
 
 	// Confirm password validation
 	if(password !== confirmPassword) {
-		errorConfirmPassword.textContent = "Passwords must be the same";
+		errorConfirmPassword.text("Passwords must be the same");
 	}
 }
